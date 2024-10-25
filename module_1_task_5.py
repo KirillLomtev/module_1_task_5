38461bc41cd0c711b130b9b20e653cea97766ccb
KirillLomtev <k.lomtev.01@gmail.com>
2024-10-25 03:11:20 +0300

    Add files via upload

---
+++ module_1_task_5.py
@@ -0,0 +1,10 @@
+immutable_var = 1, 'string', [45, 31]
+print(immutable_var)
+
+# immutable_var [1] = 2
+# print(immutable_var)
+# Кортеж - это неизменяемый список, поэтому нельзя изменить значение элементов.
+
+mutable_list = [1, 2, 3, 4, 5]
+mutable_list[0] = 99
+print(mutable_list)
